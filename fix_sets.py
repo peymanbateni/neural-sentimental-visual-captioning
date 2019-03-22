f2884925d553d309ced957c27c01484cec1c2cb1
Ubuntu <ubuntu@ip-172-31-47-1.us-west-2.compute.internal>
2019-03-22 02:39:39 +0000

    Midway commit. Preprocessing scripts added. Main notebook formed.wq

---
+++ fix_sets.py
@@ -0,0 +1,19 @@
+import os
+
+main_folder_name = "vso_images_with_cc/"
+
+for subdir in os.listdir(main_folder_name):
+
+	for filename in os.listdir(main_folder_name + subdir):
+
+		if filename.endswith("_train"):
+			os.rmdir(main_folder_name + subdir + "/" + filename)
+			print("Removed " + main_folder_name + subdir + "/" + filename)
+
+		elif filename.endswith("_validation"):
+                        os.rmdir(main_folder_name + subdir + "/" + filename)
+                        print("Removed " + main_folder_name + subdir + "/" + filename)
+
+		elif filename.endswith("_test"):
+			os.rmdir(main_folder_name + subdir + "/" + filename)
+			print("Removed " + main_folder_name + subdir + "/" + filename)
---
+++ preprocess.py
@@ -0,0 +1,75 @@
+
+from collections import Counter, defaultdict
+from IPython import display
+from nltk import word_tokenize
+from nltk.translate.bleu_score import sentence_bleu
+from PIL import Image
+from torch import nn
+from torch.autograd import Variable
+from torchvision import models, transforms
+
+import json
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import torch
+import torch.nn.functional as F
+import os
+
+img_size = 256
+loader = transforms.Compose([
+  transforms.Resize(img_size),
+  transforms.CenterCrop(img_size),
+  transforms.ToTensor(),
+])
+
+def load_image(filename, volatile=False):
+    """
+    Simple function to load and preprocess the images.
+    """
+    image = Image.open(filename).convert('RGB')
+    image_tensor = loader(image).float()
+    image_var = Variable(image_tensor, volatile=volatile).unsqueeze(0)
+    return image_var.cuda()
+
+#print(load_image('data/vso/vso_images_with_cc/amazing_flowers/1066918516_e27cbf795e.jpg'))
+
+vso_images_folder = "data/vso/vso_images_with_cc/"
+
+train_anp_tags = []
+train_image_names = []
+train_image_to_anp_tag = {}
+for subdir in os.listdir(vso_images_folder):
+	if subdir.endswith("_train"):
+		train_anp_tags.append(subdir.replace("_train", "").replace("_", " "))
+		for filename in os.listdir(vso_images_folder + subdir):
+			if filename.endswith(".jpg"):
+				train_image_names.append(vso_images_folder + subdir + "/"  + filename)
+				train_image_to_anp_tag[vso_images_folder + subdir + "/"  + filename] = subdir.replace("_train", "").replace("_", " ")
+
+validation_anp_tags = []
+validation_image_names = []
+validation_image_to_anp_tag = {}
+for subdir in os.listdir(vso_images_folder):
+        if subdir.endswith("_validation"):
+                validation_anp_tags.append(subdir.replace("_validation", "").replace("_", " "))
+                for filename in os.listdir(vso_images_folder + subdir):
+                        if filename.endswith(".jpg"):
+                                validation_image_names.append(vso_images_folder + subdir + "/"  + filename)
+                                validation_image_to_anp_tag[vso_images_folder + subdir + "/"  + filename] = subdir.replace("_validation", "").replace("_", " ")
+
+test_anp_tags = []
+test_image_names = []
+test_image_to_anp_tag = {}
+for subdir in os.listdir(vso_images_folder):
+        if subdir.endswith("_test"):
+                test_anp_tags.append(subdir.replace("_test", "").replace("_", " "))
+                for filename in os.listdir(vso_images_folder + subdir):
+                        if filename.endswith(".jpg"):
+                                test_image_names.append(vso_images_folder + subdir + "/"  + filename)
+                                test_image_to_anp_tag[vso_images_folder + subdir + "/"  + filename] = subdir.replace("_test", "").replace("_", " ")
+
+print(train_image_to_anp_tag)
+print(validation_image_to_anp_tag)
+print(test_image_to_anp_tag)
