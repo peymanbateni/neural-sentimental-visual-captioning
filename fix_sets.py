b9b5c3892fc0ac72bac375ca3b4e95f4d561a017
Peyman <peyman.bateni@hotmail.com>
2019-03-26 10:20:11 -0700

    ANP softmax classifier jupyter notebook added. Preprocessing scripts added.

--- fix_sets.py
+++ fix_sets.py
@@ -4,6 +4,21 @@ main_folder_name = "vso_images_with_cc/"
 
 for subdir in os.listdir(main_folder_name):
 
+<<<<<<< HEAD
+	for filename in os.listdir(main_folder_name + subdir):
+
+		if filename.endswith("_train"):
+			os.rmdir(main_folder_name + subdir + "/" + filename)
+			print("Removed " + main_folder_name + subdir + "/" + filename)
+
+		elif filename.endswith("_validation"):
+                        os.rmdir(main_folder_name + subdir + "/" + filename)
+                        print("Removed " + main_folder_name + subdir + "/" + filename)
+
+		elif filename.endswith("_test"):
+			os.rmdir(main_folder_name + subdir + "/" + filename)
+			print("Removed " + main_folder_name + subdir + "/" + filename)
+=======
         for filename in os.listdir(main_folder_name + subdir):
 
                 if filename.endswith("_train"):
@@ -17,3 +32,4 @@ for subdir in os.listdir(main_folder_name):
                 elif filename.endswith("_test"):
                         os.rmdir(main_folder_name + subdir + "/" + filename)
                         print("Removed " + main_folder_name + subdir + "/" + filename)
+>>>>>>> df1b56ba04016d335d3a6f3fe7e536d9c29e55a9
--- produce_sets.py
+++ produce_sets.py
@@ -2,12 +2,68 @@ import os
 import numpy as np
 import random
 
-image_dataset_folder = "vso_images_with_cc/"
+image_dataset_folder = "data/vso/vso_images_with_cc/"
 train_ratio = 0.7
 validation_ratio = 0.15
 test_ratio = 1 - (train_ratio + validation_ratio)
 
 for subdir in os.listdir(image_dataset_folder):
+<<<<<<< HEAD
+	if (not subdir.endswith("_train")) and (not subdir.endswith("_validation")) and (not subdir.endswith("_test")): 
+
+		subdir_path = image_dataset_folder + subdir + "/"
+		print("Partioning " + subdir_path)
+		number_of_images = int(len([name for name in os.listdir(subdir_path)]) / 2)
+
+		if number_of_images > 0:
+
+			train_dir_path = subdir_path[:-1] + "_train/"
+			if not os.path.exists(train_dir_path):
+				os.makedirs(train_dir_path)
+
+			validation_dir_path = subdir_path[:-1] + "_validation/"
+			if not os.path.exists(validation_dir_path):
+				os.makedirs(validation_dir_path)
+
+			test_dir_path = subdir_path[:-1] + "_test/"
+			if not os.path.exists(test_dir_path):
+				os.makedirs(test_dir_path)
+
+			number_of_train_images = int(number_of_images * train_ratio)
+			train_set_nums = random.sample(range(number_of_images), int(number_of_train_images))
+			non_train_set_nums = np.arange(number_of_images).tolist()
+
+			for item in train_set_nums:
+				non_train_set_nums.remove(item)
+
+			number_of_validation_images = int(len(non_train_set_nums) / 2)
+			validation_set_nums = non_train_set_nums[:number_of_validation_images]
+			test_set_nums = non_train_set_nums[number_of_validation_images:]
+
+			count = 0
+			for filename in os.listdir(subdir_path):
+				if filename.endswith(".jpg"):
+					filename = str(filename)
+					if count in train_set_nums:
+						os.rename(subdir_path + filename, subdir_path[:-1] + "_train/" + filename)
+						print("Moved " + subdir_path + filename + " to " + subdir_path[:-1] + "_train/" + filename)
+						os.rename(subdir_path + filename.replace(".jpg", ".json.txt"), subdir_path[:-1] + "_train/" + filename.replace(".jpg", ".json.txt"))
+						print("Moved " + subdir_path + filename.replace(".jpg", ".json.txt") + " to " + subdir_path[:-1] + "_train/" + filename.replace(".jpg", ".json.txt"))
+					elif count in validation_set_nums:
+						os.rename(subdir_path + filename, subdir_path[:-1] + "_validation/" + filename)
+						print("Moved " + subdir_path + filename + " to " + subdir_path[:-1] + "_validation/" + filename)
+						os.rename(subdir_path + filename.replace(".jpg", ".json.txt"), subdir_path[:-1] + "_validation/" + filename.replace(".jpg", ".json.txt"))
+						print("Moved " + subdir_path + filename.replace(".jpg", ".json.txt") + " to " + subdir_path[:-1] + "_validation/" + filename.replace(".jpg", ".json.txt"))
+					elif count in test_set_nums:
+						os.rename(subdir_path + filename, subdir_path[:-1] + "_test/" + filename)
+						print("Moved " + subdir_path + filename + " to " + subdir_path[:-1] + "_test/" + filename)
+						os.rename(subdir_path + filename.replace(".jpg", ".json.txt"), subdir_path[:-1] + "_test/" + filename.replace(".jpg", ".json.txt"))
+						print("Moved " + subdir_path + filename.replace(".jpg", ".json.txt") + " to " + subdir_path[:-1] + "_test/" + filename.replace(".jpg", ".json.txt"))
+					else:
+						print("Invalid instance encountered at " + subdir_path + filename)
+					count += 1
+
+=======
         if (not subdir.endswith("_train")) and (not subdir.endswith("_validation")) and (not subdir.endswith("_test")):
 
                 subdir_path = image_dataset_folder + subdir + "/"
@@ -61,3 +117,4 @@ for subdir in os.listdir(image_dataset_folder):
                                         else:
                                                 print("Invalid instance encountered at " + subdir_path + filename)
                                         count += 1
+>>>>>>> df1b56ba04016d335d3a6f3fe7e536d9c29e55a9
